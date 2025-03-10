81d9d35448efef1607db170a1729e3c08d441e88
rizaptrarz <rizaputra501@gmail.com>
2025-03-10 23:11:18 +0700

    Menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_103022300104/ProgramDoor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace tpmodul4_103022300104
+{
+    class ProgramDoor
+    {
+        static void Main()
+        {
+            DoorMachine door = new DoorMachine();
+
+            while (true)
+            {
+                Console.Write("Ketik 'buka' untuk membuka atau 'kunci' untuk mengunci (ketik 'exit' untuk keluar): ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "buka")
+                {
+                    door.BukaPintu();
+                }
+                else if (input == "kunci")
+                {
+                    door.KunciPintu();
+                }
+                else if (input == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Perintah tidak dikenali.");
+                }
+            }
+        }
+    }
+}
--- tpmodul4_103022300104/obj/Debug/net8.0/tpmodul4_103022300104.AssemblyInfo.cs
+++ tpmodul4_103022300104/obj/Debug/net8.0/tpmodul4_103022300104.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103022300104")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35840c905757be50f3578b06ad4bf11e9092f563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e41e1b342a772c14c4da9076b0ddea8c11d39e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103022300104")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103022300104")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
